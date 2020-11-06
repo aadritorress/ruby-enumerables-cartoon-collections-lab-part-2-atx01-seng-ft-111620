841535d078017dcda0c884f7eda4d3d71cc105f6
Adri Torres <adriana.amaral@msn.com>
2020-11-06 19:33:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def square_array(array)
 
   new_array = []
@@ -38,10 +38,10 @@ end
 def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
-
+binding.pry
 planeteer_calls.find { |valid_calls| valid_calls == planeteer_calls}
 
-   return planeteer_calls
+   
    
 end
 
